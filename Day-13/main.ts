a5fc389abbd76df4c4719bb97582d5f973e43db0
farhatbb <Farhatbashir77@yahoo.com>
2024-04-03 10:40:03 +0500

    Day-13-Question

---
+++ Day-13/main.ts
@@ -0,0 +1,28 @@
+//Day-13.
+//Question # 37. large Shirts.
+function make_shirt(size:string = "Large", message:string = "I love TypeScript"){
+    console.log(`Making a ${size} t-shirt with the message"${message}" printed on it.`);
+
+}
+
+make_shirt()
+make_shirt("medium")
+make_shirt("small","Dive into coding")
+
+//Question # 38 . Cities..
+function describe_City(city:string, Country: string="Paistan"){
+    console.log(`${city} is in ${Country}`);
+}
+describe_City("Karachi")
+describe_City("Islamabad")
+describe_City("Lahore")
+
+//Question #39. 
+//City Names..
+function City_Country(City:string,Country:string) : string {
+    return `${City},${Country}`
+
+}
+console.log(City_Country ("Lahore", "Pakistan"));
+console.log(City_Country("Tokyo", "Japan"));
+console.log(City_Country("New-Yark","America"));

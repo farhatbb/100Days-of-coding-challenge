95ded00902b31ced84dbab1bd71c007452225371
farhatbb <Farhatbashir77@yahoo.com>
2024-04-13 18:57:19 +0500

    Day -1 coding

---
+++ Day-1/main.js
@@ -0,0 +1,14 @@
+"use strict";
+//Question # 1.
+//install,Node.js,Typescript and Vs Code...
+//Question # 2.
+//Personal Message..
+let PersonalMessage = "Mr Asharib";
+//printing message to the person.
+console.log(`Hello! ${PersonalMessage} would you  like to learn some Python today? `);
+//Question # 3.
+//NameCase..
+let personName = "imran Khan";
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+console.log(personName.replace(/\b\w/g, (char) => char.toUpperCase()));
---
+++ Day-1/main.ts
@@ -0,0 +1,16 @@
+//Question # 1.
+//install,Node.js,Typescript and Vs Code...
+//Question # 2.
+//Personal Message..
+let PersonalMessage: string = "Mr Asharib";
+//printing message to the person.
+console.log(`Hello! ${PersonalMessage } would you  like to learn some Python today? `);
+
+
+//Question # 3.
+//NameCase..
+let personName: string = "imran Khan";
+console.log(personName.toLowerCase());
+
+console.log(personName.toUpperCase());
+console.log(personName.replace(/\b\w/g,(char) => char.toUpperCase()));

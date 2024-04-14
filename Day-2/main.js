d0c9ebfbf9add3b9492a8c1a1504f5cc6afd27d3
farhatbb <Farhatbashir77@yahoo.com>
2024-04-14 11:02:41 +0500

    Day-2 coding..

---
+++ Day-2/main.js
@@ -0,0 +1,16 @@
+"use strict";
+//Question # 4
+//famous Quote
+let famousQuote = "Setting goal is the invisible into visible.";
+console.log('tonny robbin once said,"setting goal is the invisible into visible"');
+//Question # 5.
+//famous person quote..
+let famous_person = "Tonny Robbin";
+let quote = "setting goal is the invisible into visible";
+console.log(`${famous_person} once said ${quote}`);
+//Question#6.
+//StrippingNames...
+let whitespaceName = "\n\tFarhat Bashir\t\n";
+console.log(whitespaceName);
+let withoutwhitespaceName = whitespaceName.trim();
+console.log(withoutwhitespaceName);
---
+++ Day-2/main.ts
@@ -0,0 +1,17 @@
+//Question # 4
+//famous Quote
+let famousQuote: string = "Setting goal is the invisible into visible."
+console.log('tonny robbin once said,"setting goal is the invisible into visible"');
+
+//Question # 5.
+//famous person quote..
+let famous_person: string = "Tonny Robbin";
+let quote ="setting goal is the invisible into visible";
+console.log(`${famous_person} once said ${quote}`);
+
+//Question#6.
+//StrippingNames...
+let whitespaceName: string = "\n\tFarhat Bashir\t\n";
+console.log(whitespaceName);
+let withoutwhitespaceName: string = whitespaceName.trim();
+console.log(withoutwhitespaceName);

6520df1010be93839ddbffccbd07caae903d1d14
farhatbb <Farhatbashir77@yahoo.com>
2024-04-20 17:29:27 +0500

    100-days-coding-chall

---
+++ Day-9/main.ts
@@ -0,0 +1,39 @@
+//Question #25.
+//Alien_color; #1
+let alien_color: string ='Green';
+//if statement..
+if(alien_color=== 'Green'){
+    console.log("The player just earned 5 points.");
+};
+alien_color = 'Red';
+if (alien_color === 'Green'){
+    console.log("The player just earned 5 points.");
+};
+
+//Question # 26.
+//Alien_color #2.
+let elien_color: string = 'Green';
+// if else statement.
+if(elien_color === 'Green'){
+    console.log(" The player just earned 5 points");
+} else {
+    console.log(" The player just earned 10 points");
+}
+// block else..
+elien_color = 'yellow';
+if (elien_color === 'orange'){
+    console.log("The player just earned 5 points");
+
+}else {
+    console.log("The player just earned 10 points!");
+}; 
+//Question # 27.
+let alien_Color: string = "Green";
+if (alien_Color === "Green"){
+    console.log("player earned 5 points.");
+}else if( alien_Color === "yellow"){
+    console.log("player earned 10 points.");
+
+}else if (alien_Color === "red"){
+    console.log("player earned 15 points");
+}

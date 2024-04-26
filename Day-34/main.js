64f7734a473d3a2b168c8f368a14956b7419d1eb
farhatbb <Farhatbashir77@yahoo.com>
2024-04-26 20:40:28 +0500

    100-Days-coding-chall

---
+++ Day-34/main.js
@@ -0,0 +1,17 @@
+"use strict";
+//Question #100
+//Find the square root of 144.
+let number = 144;
+let squareRoot = Math.sqrt(number);
+console.log(squareRoot); // out put: 12
+//Question# 101
+//Generate a random integer between 1 and 10.
+let randomNumber = Math.random();
+let randomInteger = Math.floor(randomNumber * 10) + 1;
+console.log(randomInteger);
+//Question#102
+//calculat and log the absolute difference between the number -5 and 5.Math.abs()
+let number1 = -5;
+let number2 = 5;
+let absoluteDifference = Math.abs(number1 - number2);
+console.log(absoluteDifference);

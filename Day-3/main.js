7f786726dc885de26f89a598cbe6ce2bafdb8cde
farhatbb <Farhatbashir77@yahoo.com>
2024-05-13 11:20:06 +0500

    100-Days-chall

---
+++ Day-3/main.js
@@ -0,0 +1,14 @@
+"use strict";
+//Question # 7 & 8
+// write add,sub,multi,divi opeeration..
+let num1 = 5;
+let num2 = 3;
+let add = num1 + num2;
+console.log(add);
+console.log(11 - 3);
+console.log(4 * 2);
+console.log(16 / 2);
+//Question # 9
+// favorite number store..
+let favoriteNumber = 4;
+console.log(`My favorite number is. ${favoriteNumber}`);
---
+++ Day-3/main.ts
@@ -0,0 +1,14 @@
+//Question # 7 & 8
+// write add,sub,multi,divi opeeration..
+let num1: number = 5;
+let num2: number = 3;
+let add: number = num1 +num2;
+console.log(add);
+console.log(11-3);
+console.log(4*2);
+console.log(16/2);
+
+//Question # 9
+// favorite number store..
+let favoriteNumber: number = 4;
+console.log(`My favorite number is. ${favoriteNumber}`);

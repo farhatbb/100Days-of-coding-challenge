84fdeaac9db98b47444955a37ece34427d018ae8
farhatbb <Farhatbashir77@yahoo.com>
2024-05-27 17:38:10 +0500

    100-Days- coding-chall..

---
+++ Day-40/main.js
@@ -0,0 +1,17 @@
+"use strict";
+//Day#40 Loops.
+//Question #118
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+//Question#119.
+let count = 0;
+while (count < 5) {
+    console.log("Hello World!");
+    count++;
+}
+//Question120.
+let favoriteMovies = ["The Dark Knight", "Puple Fiction", "The Martix"];
+for (let movie of favoriteMovies) {
+    console.log(movie);
+}
---
+++ Day-40/main.ts
@@ -0,0 +1,17 @@
+//Day#40 Loops.
+//Question #118
+for (let i = 1 ; i <=10; i++){
+    console.log(i);
+
+}
+//Question#119.
+let count = 0;
+while (count < 5){
+    console.log("Hello World!");
+    count++;
+}
+//Question120.
+let favoriteMovies: string[]= ["The Dark Knight","Puple Fiction","The Martix"];
+for(let movie of favoriteMovies){
+    console.log(movie);
+}

c8431eee016da03f443421ec5d7f1ae1ce84b990
farhatbb <Farhatbashir77@yahoo.com>
2024-06-07 21:44:56 +0500

    100-Days-coding-chall

---
+++ Day-4/main.js
@@ -0,0 +1,28 @@
+"use strict";
+//Day 4,question #10
+//Adding Comments.
+//Author: [Farhat]
+//storing person name in a variable.
+let personName = "Ahmed";
+// printing a message to theperson.
+console.log(`Hello ${personName}, would you like to learn some python today?`);
+// Author:[Farhat]
+// assign a  my favorite number in  variable.
+let favNum = 3;
+// printing my favourite number in amessage
+console.log(`My favorite number is ${favNum}.`);
+//question 11.
+//name store in array.
+let names = ["Hamna", "Momina", "Aqsa", "Ayesha"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+//question#12
+//Greatting.
+let names2 = ["Hamna", "Momina", "Aqsa", "Ayesha"];
+let message = "would you like to learn typescript?";
+console.log(names2[0] + "" + message);
+console.log(names2[1] + "" + message);
+console.log(names2[2] + "" + message);
+console.log(names2[3] + "" + message);
